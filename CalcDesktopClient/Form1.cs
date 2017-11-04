fba1019a85669f5ba4362f81815ee89d2489627d
Клименко Владимир <klimbox@gmail.com>
2017-11-05 00:31:54 +0200

    #1: Initial commit. Added calculator client - desktop version.

---
+++ CalcDesktopClient/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
+
+namespace CalcDesktopClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HttpClient httpC;
+        string fNum;
+        string sNum;
+        string op;
+        string url = "http://localhost:8080/";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            httpC = new HttpClient();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text += ((Button)sender).Text;
+            }
+            catch (InvalidCastException)
+            { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                fNum = textBox1.Text;
+                op = ((Button)sender).Text;
+                if (op == "+") { op = "plus"; }
+                textBox1.Text = "";
+            }
+        }
+
+        private async void button17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                sNum = textBox1.Text;
+                try
+                {
+                    textBox1.Text = await httpC.GetStringAsync($"{url}?num1={fNum}&num2={sNum}&opr={op}");
+                }
+                catch(HttpRequestException)
+                {
+                    textBox1.Text = "server not respond";
+                }
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            fNum = sNum = op = null;
+            this.textBox1.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string txt = textBox1.Text;
+            textBox1.Text = txt.Substring(0, txt.Length - 1);
+        }
+    }
+}
